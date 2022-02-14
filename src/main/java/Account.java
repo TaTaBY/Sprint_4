6c45cc80cd5244c1a08a5a74c59dccd645545b4f
Maksim Tatachka <maksim.tatachka@appcast.io>
2022-02-14 20:43:40 +0300

    update after review

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Account {
 
     private final String name;
@@ -11,15 +14,20 @@ public class Account {
              Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
              Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
          */
-        try {
-            return name.matches("^(?=.{3,19}$)([\\S]+\\s[\\S]+)$");
-            //old version
-            //name.length() >= 3 && name.length() <= 19 && name.contains(" ") && !name.startsWith(" ")
-            //&& !name.endsWith(" ") && name.indexOf(" ") == name.lastIndexOf(" ");
-        } catch (Exception e) {
+        if (name == null) {
             return false;
         }
 
+        String regex = "^(?=.{3,19}$)([a-zA-ZА-Яа-я]+\\s[a-zA-ZА-Яа-я+-]+)$";
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(name);
+
+        return matcher.matches();
+        //return name.matches("^(?=.{3,19}$)([a-zA-ZА-Яа-я]+\\s[a-zA-ZА-Яа-я+-]+)$");
+            //old version
+            //name.length() >= 3 && name.length() <= 19 && name.contains(" ") && !name.startsWith(" ")
+            //&& !name.endsWith(" ") && name.indexOf(" ") == name.lastIndexOf(" ");
     }
 
 }
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -21,6 +21,8 @@ public class AccountTest {
     public static Object[][] getTestData() {
         return new Object[][] {
                 {"Тимоте Шаламе", true},
+                {"Тим оте-аламе", true},
+                {"Тим1 оте-аламе", false},
                 {"ТимотеШаламе", false},
                 {"Ти", false},
                 {"Тимоте Шаламетесттес", false},
